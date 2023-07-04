5f2350c2ee64dad5416fc15c08a54f0de941b906
Nirmeet Gandhi <85167970+NirmeetGandhi@users.noreply.github.com>
2023-07-04 23:44:32 +0530

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int largestNumber;
+
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                largestNumber = num1;
+            } else {
+                largestNumber = num3;
+            }
+        } else {
+            if (num2 >= num3) {
+                largestNumber = num2;
+            } else {
+                largestNumber = num3;
+            }
+        }
+
+        System.out.println("The longest no is: " + largestNumber);
+    }
+}
