20651ae80b1f41ad8c03bf5c968086029ed92253
Nirmeet Gandhi <85167970+NirmeetGandhi@users.noreply.github.com>
2023-07-04 23:48:50 +0530

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,11 @@
+public class Q5{
+  public static void main(String[]args){
+    int a=100;
+    double b=5.55;
+    String c= "Messi";
+    char d='a';
+    float f=1.1f;
+    boolean b1= true;
+  }
+  
+}
